f9d5bd1a14b09f70220a380e31790b0ecc3a578d
greatraksin <greatraksin@icloud.com>
2023-05-25 16:14:35 +0300

    Adding login view class

--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from .forms import UserLoginForm, UserRegisterForm
+from django.contrib.auth import logout
+from .forms import UserRegisterForm
 # Create your views here.
-
-
-def login_view(request):
-    form = UserLoginForm(request.POST)
-    if form.is_valid():
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password')
-        user = authenticate(request, username=username,
-                            password=password)
-        if user is not None:
-            login(request, user)  # логиним
-            return redirect("home")
-    return render(request, "form.html", {"form": form,
-                                         "title": "Login"})
 
 
 def logout_view(request):
--- online_car_rental_system_Django/urls.py
+++ online_car_rental_system_Django/urls.py
@@ -17,12 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from accounts.views import login_view, logout_view, register_view
+from accounts.views import logout_view, register_view
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ocrs.urls')),
-    path('login/', login_view, name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='form.html',
+                                                redirect_authenticated_user=True,
+                                                extra_content={'title': 'Login'}), name='login'),
     path('logout/', logout_view, name='logout'),
     path('register/', register_view, name='register'),
 ]
